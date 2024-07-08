87d85571b1fafb93f7554b0f8b92bf353f28d3b0
BrayanBJ27 <xavi_braian@hotmail.com>
2024-07-07 20:44:41 -0500

    feat: :bug: Fix index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
-
 const app = express();
-const port = 8089
-;
+const port = process.env.Por || 8089;
 
 // Middleware
 app.use(cors());
@@ -12,10 +10,10 @@ app.use(express.json());
 
 // Configure the database connection
 const db = mysql.createConnection({
-    host: 'mysql-programaciondis.alwaysdata.net',
-    user: '357676_bj',
-    password: 'Uyt:tBHLgt4Kk_E',
-    database: 'programaciondis_gym'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {
@@ -26,9 +24,46 @@ db.connect((err) => {
     console.log('Connected to the MySQL database.');
 });
 
-// Import routes
-const machineReservationsRoutes = require('./routes/machineReservations')(db);
-app.use('/reservations', machineReservationsRoutes);
+// Route to get the list of clients
+app.get('/reservations', (req, res) => {
+    const selectQuery = 'SELECT * FROM machine_reservations';
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Error fetching reservations:', err);
+            res.status(500).send({ message: 'Error fetching reservations', error: err });
+            return;
+        }
+        const formattedResults = results.map((reservation) => {
+            let startTime = null;
+            let endTime = null;
+
+            if (reservation.start_time) {
+                try {
+                    startTime = new Date(reservation.start_time).toISOString().replace('T', ' ').substring(0, 19);
+                } catch (error) {
+                    console.error('Invalid start_time value:', reservation.start_time);
+                }
+            }
+                
+            if (reservation.end_time) {
+                try {
+                    endTime = new Date(reservation.end_time).toISOString().replace('T', ' ').substring(0, 19);
+                } catch (error) {
+                    console.error('Invalid end_time value:', reservation.end_time);
+                }
+            }
+                
+            return {
+                id: reservation.id,
+                machineName: reservation.machine_name,
+                userName: reservation.user_name,
+                startTime,
+                endTime
+            };
+        });
+        res.send(formattedResults);
+    });
+});
 
 // Root route to check if the server is running
 app.get('/', (req, res) => {
