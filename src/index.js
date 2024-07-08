e650288afc1ba8915def17cbfe15632dbff24d95
BrayanBJ27 <xavi_braian@hotmail.com>
2024-07-07 20:57:53 -0500

    feat: :bug: Fix index.js

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ db.connect((err) => {
     console.log('Connected to the MySQL database.');
 });
 
-// Route to get the list of clients
+// Route to get the list of reservations
 app.get('/reservations', (req, res) => {
     const selectQuery = 'SELECT * FROM machine_reservations';
     db.query(query, (err, results) => {
